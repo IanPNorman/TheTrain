bbbbdefccf10b7467cccea0928e7251352cc563a
IanPNorman <ianpk7211@gmail.com>
2025-02-13 10:25:27 -0800

    TrainMoveTest

---
+++ Assets/Scripts/Player/BasicMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        transform.position += move * speed * Time.deltaTime;
+
+    }
+}
